48babbd733f0236a8ccfcacb4279105b0c41fbd3
jumpinghooligans <ryankortmann@gmail.com>
2014-03-06 12:05:32 -0500

    Update receiver_server.js

--- public/javascripts/receiver_server.js
+++ public/javascripts/receiver_server.js
@@ -2,3 +2,6 @@ var socket = io.connect('http://chromecast-against-humanity.herokuapp.com/');
 //console.log(navigator.userAgent);
 
 socket.emit('message', { userAgent : navigator.userAgent });
+
+socket.emit('message', { userAgent : "Pipeline Successful" });
+
